39b84d4771390702d705585a9bc482adb9ef5395
Aaron <Carl.aaron.tage.carlsson@gmail.com>
2026-03-02 18:57:13 +0100

    Change console log to alert for menu update notice

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-console.log("New update for Dashmetry broke the menu (they added a hidden patch) but i will fix it asap. Just have some patience because I dont have enough time now.");
+alert("New update for Dashmetry broke the menu (they added a hidden patch) but i will fix it asap. Just have some patience because I dont have enough time now.");
